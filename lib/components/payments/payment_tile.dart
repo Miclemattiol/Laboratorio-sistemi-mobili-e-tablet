9f6ed66b212c465e17a0c7b04e84ebe0b2e2a9f3
Michael Mattiolo <miclemattiol@gmail.com>
2023-05-24 20:39:51 +0200

    Now PaymentTile supports names and signed balance impact

--- lib/components/payments/payment_tile.dart
+++ lib/components/payments/payment_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_series/flutter_series.dart';
 import 'package:house_wallet/data/firestore.dart';
+import 'package:house_wallet/data/logged_user.dart';
 import 'package:house_wallet/data/payments/payment.dart';
 import 'package:house_wallet/main.dart';
 
@@ -11,9 +12,31 @@ class PaymentTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    num impact;
+    print(LoggedUser.uid);
+    if (doc.data.from == LoggedUser.uid) {
+      num nParts = 0;
+      doc.data.to.forEach((key, value) {
+        nParts += value;
+      });
+      if (doc.data.to.containsKey(LoggedUser.uid)) {
+        impact = (doc.data.price / nParts) * (nParts - doc.data.to[LoggedUser.uid]!);
+      } else {
+        impact = doc.data.price;
+      }
+    } else if (doc.data.to.containsKey(LoggedUser.uid)) {
+      num nParts = 0;
+      doc.data.to.forEach((key, value) {
+        nParts += value;
+      });
+      impact = -(doc.data.price / nParts) * doc.data.to[LoggedUser.uid]!;
+    } else {
+      impact = 0;
+    }
+
     return ListTile(
       title: Text(doc.data.title),
-      subtitle: Text(localizations(context).paymentPaidFrom(doc.data.from)),
+      subtitle: FutureBuilder(future: FirestoreData.getUser(doc.data.from), builder: (context, snapshot) => Text(localizations(context).paymentPaidFrom(snapshot.data!.username))),
       leading: const SizedBox(height: double.infinity, child: Icon(Icons.shopping_cart)),
       trailing: PadColumn(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -22,7 +45,7 @@ class PaymentTile extends StatelessWidget {
         children: [
           Text(currencyFormat(context).format(doc.data.price)),
           Text(
-            localizations(context).paymentPaidImpact(currencyFormat(context).format(0)),
+            localizations(context).paymentPaidImpact(currencyFormat(context).format(impact)),
             style: const TextStyle(fontSize: 10),
           ),
         ],
