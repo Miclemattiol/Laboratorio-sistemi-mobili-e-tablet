91c875f0c81045a8b3d54fd98db9935326aa4b41
Loris Giuliani <4loris4@gmail.com>
2023-05-25 23:19:09 +0200

    List and ListItems for the Shopping section Added DetailsItemChips for Shopping's input

--- lib/pages/shopping/shopping_page.dart
+++ lib/pages/shopping/shopping_page.dart
@@ -16,39 +16,49 @@ class ShoppingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarFix(title: Text(localizations(context).shoppingPage)),
-      body: StreamBuilder(
-        stream: ShoppingPage.firestoreRef.snapshots().asyncMap(ShoppingItemRef.converter),
-        builder: (context, snapshot) {
-          final shoppingItems = snapshot.data;
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder(
+              stream: ShoppingPage.firestoreRef.snapshots().asyncMap(ShoppingItemRef.converter),
+              builder: (context, snapshot) {
+                final shoppingItems = snapshot.data;
 
-          if (shoppingItems == null) {
-            //TODO error and loading
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: Text("Loading..."));
-            } else {
-              return Center(child: Text("Error (${snapshot.error})"));
-            }
-          }
+                if (shoppingItems == null) {
+                  //TODO error and loading
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: Text("Loading..."));
+                  } else {
+                    return Center(child: Text("Error (${snapshot.error})"));
+                  }
+                }
 
-          if (shoppingItems.isEmpty) {
-            //TODO empty message
-            return const Center(child: Text("No data"));
-          }
+                if (shoppingItems.isEmpty) {
+                  //TODO empty message
+                  return const Center(child: Text("No data"));
+                }
 
-          return SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Container(
-                color: const Color(0xFFE6D676),
-                child: Column(
-                  children: shoppingItems.map(ShoppingItemTile.new).toList(),
-                ),
-              ),
+                return SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                    child: Container(
+                      clipBehavior: Clip.antiAlias,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
+                        color: Color(0xFFE6D676),
+                      ), //TODO color theme
+                      child: Column(
+                        children: List.filled(20, shoppingItems.first).map(ShoppingItemTile.new).toList(),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+          const ShoppingBottomSheet()
+        ],
       ),
-      bottomSheet: const ShoppingBottomSheet(),
     );
   }
 }
