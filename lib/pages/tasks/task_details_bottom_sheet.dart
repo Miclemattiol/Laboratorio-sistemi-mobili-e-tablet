ac06b067223dfcff2c8ceb17b5a2e05bbce2f66f
Michael Mattiolo <miclemattiol@gmail.com>
2023-05-26 16:15:36 +0200

    Importato datepickerformfield

--- lib/pages/tasks/task_details_bottom_sheet.dart
+++ lib/pages/tasks/task_details_bottom_sheet.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_series/flutter_series.dart';
+import 'package:house_wallet/components/form/date_picker_form_field.dart';
 import 'package:house_wallet/components/ui/custom_bottom_sheet.dart';
 import 'package:house_wallet/components/ui/modal_button.dart';
 import 'package:house_wallet/components/ui/user_avatar.dart';
@@ -32,6 +33,7 @@ class _TaskDetailsBottomSheetState extends State<TaskDetailsBottomSheet> {
   bool _edited = false;
 
   TextEditingController fromDateController = TextEditingController();
+  TextEditingController toDateController = TextEditingController();
 
   _saveTask() async {
     if (!_edited) return;
@@ -46,31 +48,13 @@ class _TaskDetailsBottomSheetState extends State<TaskDetailsBottomSheet> {
         spacing: 16,
         body: [
           TextFormField(
-            decoration: inputDecoration("Title"),
+            decoration: inputDecoration(localizations(context).title),
           ),
-          PadRow(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            spacing: 8,
-            children: [
-              const Text("Data inizio:", style: TextStyle(fontSize: 16)),
-              Expanded(
-                child: TextFormField(
-                  decoration: inputDecoration("Data inizio:"),
-                  controller: fromDateController,
-                  readOnly: true,
-                  onTap: () async {
-                    final date = await showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2000), lastDate: DateTime(2101));
-                    if (date == null) return;
-                    String formattedDate = DateFormat('dd/MM/yyyy').format(date);
-                    fromDateController.text = formattedDate;
-                    setState(() {
-                      fromDateController.text = formattedDate;
-                      _edited = true;
-                    });
-                  },
-                ),
-              ),
-            ],
+          DatePickerFormField.dateOnly(
+            decoration: inputDecoration("Data inizio"),
+          ),
+          DatePickerFormField.dateOnly(
+            decoration: inputDecoration("Data fine"),
           ),
         ],
         actions: [
@@ -78,6 +62,45 @@ class _TaskDetailsBottomSheetState extends State<TaskDetailsBottomSheet> {
           ModalButton(onPressed: _saveTask, child: Text(localizations(context).buttonOk)),
         ],
       ),
+    );
+  }
+}
+
+class DateInputField extends StatefulWidget {
+  const DateInputField({super.key});
+
+  @override
+  State<DateInputField> createState() => _DateInputFieldState();
+}
+
+class _DateInputFieldState extends State<DateInputField> {
+  TextEditingController dateController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return PadRow(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      spacing: 8,
+      children: [
+        const SizedBox(width: 96, child: Text("Data fine:", style: TextStyle(fontSize: 16))),
+        Expanded(
+          child: TextFormField(
+            decoration: inputDecoration(DateFormat('dd/MM/yyyy').format(DateTime.now())),
+            controller: dateController,
+            readOnly: true,
+            onTap: () async {
+              final date = await showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2000), lastDate: DateTime(2101));
+              if (date == null) return;
+              String formattedDate = DateFormat('dd/MM/yyyy').format(date);
+              dateController.text = formattedDate;
+              setState(() {
+                dateController.text = formattedDate;
+              });
+            },
+          ),
+        ),
+      ],
     );
   }
 }
